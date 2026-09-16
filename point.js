b977bd828becd2322f33b830c542e507e0b048e5
mnepohhui <ablyaz34@proton.me>
2026-09-16 23:30:49 +0300

    homesavefinal

--- point.js
+++ point.js
@@ -1,16 +1,58 @@
-const pointcont = document.querySelector('.pointcont');
+const crsrcrcont = document.querySelector('.cursorcont');
+const crsrcr = document.querySelector('.cursorcircle');
 
-document.addEventListener('mousemove', (event) => {
-  pointcont.style.left = event.clientX + 'px';
-  pointcont.style.top = event.clientY + 'px';
+const pic = document.querySelector('.gridpic');
+const starArea1 = document.querySelector('.grid_item1');
+
+const str = document.querySelector('.gridstar');
+const starArea2 = document.querySelector('.grid_item2');
+
+
+function moveCircle(e) {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  crsrcrcont.style.transform = `translate(${x}px, ${y}px)`;
+
+  const rect1 = starArea1.getBoundingClientRect();
+  pic.style.transform =
+    `translate(${x - rect1.left - 45}px, ${y - rect1.top - 70}px)`;
+
+  const rect2 = starArea2.getBoundingClientRect();
+  str.style.transform =
+    `translate(${x - rect2.left - 150}px, ${y - rect2.top - 150}px)`;
+
+}
+
+document.onmousemove = moveCircle;
+
+
+
+
+
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+
+line1.addEventListener('mouseenter', () => {
+  crsrcr.classList.add('line1cursor');
+  crsrcr.innerHTML = '<span style="color: white; font-size:30px">1</span>'
 });
 
-document.addEventListener('mousemove', (event) => {
-  document.querySelector('.vertical_line').style.left = event.clientX + 'px';
-  document.querySelector('.vertical_line').style.top = event.clientY + 'px';
+line1.addEventListener('mouseleave', () => {
+  crsrcr.classList.remove('line1cursor');
+  crsrcr.innerHTML = '';
 });
 
-document.addEventListener('mousemove', (event) => {
-  document.querySelector('.horizontal_line').style.left = event.clientX + 'px';
-  document.querySelector('.horizontal_line').style.top = event.clientY + 'px';
+
+
+line2.addEventListener('mouseenter', () => {
+  crsrcr.classList.add('line2cursor');
+  crsrcr.innerHTML = '<span style="color: white; font-size:30px">2</span>'
 });
+
+line2.addEventListener('mouseleave', () => {
+  crsrcr.classList.remove('line2cursor');
+  crsrcr.innerHTML = '';
+});
+
+
